e89e35ea526c5f561dfd21636475043146402f3a
Rémy Virin <remy@remyvirin.com>
2018-03-06 14:49:19 +0100

    Add streamActivity, stream from camera with libVLC

--- app/src/main/java/com/onvif/onvifcamera_android/MainActivity.kt
+++ app/src/main/java/com/onvif/onvifcamera_android/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.onvif.onvifcamera_android
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.TextView
 import com.onvif.onvifcamera_android.Onvif.OnvifDevice
 import com.onvif.onvifcamera_android.Onvif.OnvifRequest
 import com.onvif.onvifcamera_android.Onvif.OnvifUI
 import com.onvif.onvifcamera_android.Onvif.currentDevice
 
+const val RTSP_URL = "com.onvif.onvifcamera_android.RTSP_URL"
 
 class MainActivity : AppCompatActivity(), OnvifUI {
 
@@ -19,12 +22,12 @@ class MainActivity : AppCompatActivity(), OnvifUI {
 
         //currentDevice = OnvifDevice("60.191.94.122:8086","admin", "admin321")
 
-        currentDevice = OnvifDevice("60.191.94.122:8086","admin", "admin321")
+        currentDevice = OnvifDevice("60.191.94.122:8086", "admin", "admin321")
         currentDevice.delegate = this
 
-       // currentDevice.getDeviceInformation()
+        // currentDevice.getDeviceInformation()
         currentDevice.getProfiles()
-       // currentDevice.getStreamURI()
+        // currentDevice.getStreamURI()
     }
 
     override fun requestPerformed(requestType: OnvifRequest.Type, uiMessage: String) {
@@ -36,5 +39,17 @@ class MainActivity : AppCompatActivity(), OnvifUI {
         } else if (requestType == OnvifRequest.Type.GetProfiles) {
             currentDevice.getStreamURI()
         }
+    }
+
+
+    fun openStreamIntent(view: View) {
+        val url = "rtsp://admin:admin321@60.191.94.122:554/cam/realmonitor?channel=1&subtype=1&unicast=true&proto=Onvif"
+        //val url = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov"
+
+        val intent = Intent(this, StreamActivity::class.java).apply {
+            putExtra(RTSP_URL, url)
+        }
+
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/onvif/onvifcamera_android/StreamActivity.kt
@@ -0,0 +1,60 @@
+package com.onvif.onvifcamera_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.SurfaceView
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.pedro.vlc.VlcListener
+import com.pedro.vlc.VlcVideoLibrary
+
+class StreamActivity : AppCompatActivity(), VlcListener, View.OnClickListener {
+
+
+    var vlcVideoLibrary: VlcVideoLibrary? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stream)
+
+        val surfaceView = findViewById<SurfaceView>(R.id.surfaceView)
+        val bStartStop = findViewById<Button>(R.id.b_start_stop)
+        bStartStop.setOnClickListener(this)
+        vlcVideoLibrary = VlcVideoLibrary(this, this, surfaceView)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val url = intent.getStringExtra(RTSP_URL)
+      //  vlcVideoLibrary?.play(url)
+    }
+
+    override fun onComplete() {
+        Toast.makeText(this, "Playing", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onError() {
+        Toast.makeText(this, "Error, make sure your endpoint is correct", Toast.LENGTH_SHORT).show()
+        vlcVideoLibrary?.stop()
+    }
+
+
+    override fun onClick(v: View?) {
+
+        vlcVideoLibrary?.let { vlcVideoLibrary ->
+
+            if (!vlcVideoLibrary.isPlaying) {
+                val url = intent.getStringExtra(RTSP_URL)
+                vlcVideoLibrary.play(url)
+
+            } else {
+                vlcVideoLibrary.stop()
+
+            }
+        }
+    }
+}
+
