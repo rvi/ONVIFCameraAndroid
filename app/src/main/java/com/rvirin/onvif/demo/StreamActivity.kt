a354877a6363c1051c3a9168dcb44fe6668557f1
Sean Proctor <sproctor@gmail.com>
2021-01-06 15:46:03 -0500

    Switch to official LibVLC

--- app/src/main/java/com/rvirin/onvif/demo/StreamActivity.kt
+++ app/src/main/java/com/rvirin/onvif/demo/StreamActivity.kt
@@ -1,62 +1,56 @@
 package com.rvirin.onvif.demo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-import android.view.SurfaceView
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
-import com.pedro.vlc.VlcListener
-import com.pedro.vlc.VlcVideoLibrary
+import androidx.appcompat.app.AppCompatActivity
 import com.rvirin.onvif.R
+import org.videolan.libvlc.LibVLC
+import org.videolan.libvlc.Media
+import org.videolan.libvlc.MediaPlayer
+import org.videolan.libvlc.util.VLCVideoLayout
 
 /**
  * This activity helps us to show the live stream of an ONVIF camera thanks to VLC library.
  */
-class StreamActivity : AppCompatActivity(), VlcListener, View.OnClickListener {
+class StreamActivity : AppCompatActivity() {
 
-
-    private var vlcVideoLibrary: VlcVideoLibrary? = null
+    private lateinit var libVLC: LibVLC
+    private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stream)
 
-        val surfaceView = findViewById<SurfaceView>(R.id.surfaceView)
-        val bStartStop = findViewById<Button>(R.id.b_start_stop)
-        bStartStop.setOnClickListener(this)
-        vlcVideoLibrary = VlcVideoLibrary(this, this, surfaceView)
+        libVLC = LibVLC(this)
+        mediaPlayer = MediaPlayer(libVLC)
     }
 
-    /**
-     * Called by VLC library when the video is loading
-     */
-    override fun onComplete() {
-        Toast.makeText(this, "Loading video...", Toast.LENGTH_LONG).show()
+    override fun onStart() {
+        super.onStart()
+
+        val videoView = findViewById<VLCVideoLayout>(R.id.video_view)
+        mediaPlayer.attachViews(videoView, null, false, false)
+
+        val url = intent.getStringExtra(RTSP_URL)
+        val uri = Uri.parse(url)
+        val media = Media(libVLC, uri)
+        mediaPlayer.media = media
+        media.release()
+        mediaPlayer.play()
     }
 
-    /**
-     * Called by VLC library when an error occured (most of the time, a problem in the URI)
-     */
-    override fun onError() {
-        Toast.makeText(this, "Error, make sure your endpoint is correct", Toast.LENGTH_SHORT).show()
-        vlcVideoLibrary?.stop()
+    override fun onDestroy() {
+        super.onDestroy()
+
+        mediaPlayer.release()
+        libVLC.release()
     }
 
+    override fun onStop() {
+        super.onStop()
 
-    override fun onClick(v: View?) {
-
-        vlcVideoLibrary?.let { vlcVideoLibrary ->
-
-            if (!vlcVideoLibrary.isPlaying) {
-                val url = intent.getStringExtra(RTSP_URL)
-                vlcVideoLibrary.play(url)
-
-            } else {
-                vlcVideoLibrary.stop()
-
-            }
-        }
+        mediaPlayer.stop()
+        mediaPlayer.detachViews()
     }
 }
 
